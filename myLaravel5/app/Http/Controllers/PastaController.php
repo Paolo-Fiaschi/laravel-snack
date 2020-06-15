25d0aee3145c650edc7e72971910a246466e55e0
Paolo <p.fiaschi@hotmail.it>
2020-06-15 16:06:10 +0200

    db caricato su phpmyadmin

--- myLaravel5/app/Http/Controllers/PastaController.php
+++ myLaravel5/app/Http/Controllers/PastaController.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Pasta;
 use Illuminate\Http\Request;
 
 class PastaController extends Controller
 {
     public function index()
     {
-        $cards = config('pasta');
-        $collection = collect($cards);
+        // $cards = config('pasta');
+        // $collection = collect($cards);
+
+        $collection = Pasta::all();
         $lunga = $collection -> where('tipo', 'lunga');
         $corta = $collection -> where('tipo', 'corta');
         $cortissima = $collection -> where('tipo', 'cortissima');
--- myLaravel5/routes/console.php
+++ myLaravel5/routes/console.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Pasta;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+$this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('fillPastaDB', function () {
+    $cards = config('pasta');
+    foreach ($cards as $card) {
+
+        $pasta = new Pasta;
+
+        $pasta -> src = $card['src'];
+        $pasta -> titolo = $card['titolo'];
+        $pasta -> tipo = $card['tipo'];
+        $pasta -> cottura = $card['cottura'];
+        $pasta -> peso = $card['peso'];
+        $pasta -> descrizione = $card['descrizione'];
+
+        $pasta -> save();
+    }
+})->describe('Fill PastaDB from config db');

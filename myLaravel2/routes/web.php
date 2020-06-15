cfbd55f76a39df8b64aa554652fb6e79d6c11ba5
Paolo <p.fiaschi@hotmail.it>
2020-06-15 11:26:07 +0200

    creazione db mysql

--- myLaravel2/routes/web.php
+++ myLaravel2/routes/web.php
@@ -4,17 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    $cards  = config('pasta');
-    $collection = collect($cards)->map(function($item, $key){
-        $item['id'] = $key;
-        return $item;
-    });
-    $lunga = $collection -> where('tipo', 'lunga');
-    $corta = $collection -> where('tipo', 'corta');
-    $cortissima = $collection -> where('tipo', 'cortissima');
 
-
-    return view('home', compact('lunga', 'corta', 'cortissima'));
+    return view('home');
 }) ->name('home');
 Route::get('/showPasta', function () {
     $cards  = config('pasta');

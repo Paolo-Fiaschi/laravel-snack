800752a27a7323a6dad5c0031be4c7dff38cd677
Paolo <p.fiaschi@hotmail.it>
2020-06-22 18:34:07 +0200

    creati tutti i file

---
+++ myLaravel8/database/factories/PostsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'name' => $faker -> name(),
+        'title' => $faker -> word(),
+        'description' => $faker -> sentence()
+    ];
+});
